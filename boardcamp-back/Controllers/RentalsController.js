4fa0ecf9c36bd3a1ace1eebcec4066055376f553
Raphael Braga <rbragamouraa@gmail.com>
2022-10-03 16:41:30 -0300

    CRUD Rentals Post

--- boardcamp-back/Controllers/RentalsController.js
+++ boardcamp-back/Controllers/RentalsController.js
@@ -1,20 +1,20 @@
 import connection from "../src/db/db.js";
 import SchemaRentals from "../Schemas/RentalSchema.js"
-import dayjs from "dayjs";
-
-const tempo=dayjs().format('HH:MM:ss')
 
 
-export async function GetRentals(req,res){
-    const { CustomerId, GameId} = req.query
-    try{
-        const params =[]
-        const conditions = []
+
+
+
+// export async function GetRentals(req,res){
+//     const { CustomerId, GameId} = req.query
+//     try{
+//         const params =[]
+//         const conditions = []
        
-    }catch(error){
+//     }catch(error){
 
-    }
-}
+//     }
+// }
 
 export async function PostRentals(req,res){
     const InsertRental = req.body
@@ -26,23 +26,29 @@ export async function PostRentals(req,res){
         }
 
         const searchId = await connection.query('SELECT * FROM customers WHERE id = $1',[InsertRental.customerId])
+  
         const searchGameId = await connection.query('SELECT * FROM games WHERE id = $1',[InsertRental.gameId])
+       
         if(!searchId || !searchGameId){
             return res.sendStatus(400)
         }
 
        
-      
-        const originalPrice = searchGameId.pricePerDay * InsertRental.daysRented
         
+        const originalPrice = searchGameId.rows[0].pricePerDay * Number(InsertRental.daysRented)
+        
+        console.log(InsertRental.daysRented)
 
-        await connection.query('INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, null, $5, null)'[
+        await connection.query(`INSERT INTO rentals (
+            "customerId", "gameId", "rentDate", 
+            "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, NOW(), $3, null, $4, null)`,[
             InsertRental.customerId,
             InsertRental.gameId,
-            tempo,
             InsertRental.daysRented,
             originalPrice
             ])
+
+          
 
        return res.sendStatus(201)
 
--- boardcamp-back/routes/Rentals.js
+++ boardcamp-back/routes/Rentals.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { GetRentals, PostRentals } from '../Controllers/RentalsController.js';
+import {  PostRentals } from '../Controllers/RentalsController.js';
 
 const router = express.Router()
 
-router.get('/rentals', (GetRentals))
+//router.get('/rentals', (GetRentals))
 router.post('/rentals', (PostRentals))
 
 export default router;
